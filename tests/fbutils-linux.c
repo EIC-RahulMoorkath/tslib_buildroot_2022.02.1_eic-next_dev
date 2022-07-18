061574c5ace7df4801a697121e0c4ecb86cc5804
rahul.moorkoth <rahul.moorkoth@einfochips.com>
2022-07-18 12:55:33 +0530

    tslib v1.22: Added crosshairs changes
    Signed-off-by: rahul.moorkoth <rahul.moorkoth@einfochips.com>

--- tests/fbutils-linux.c
+++ tests/fbutils-linux.c
@@ -224,7 +224,26 @@ void put_cross(int32_t x, int32_t y, uint32_t colidx)
 	}
 }
 
-static void put_char(int32_t x, int32_t y, int32_t c, int32_t colidx)
+void put_crosshairs(int x, int y, unsigned colidx)
+{
+
+	line (x-50, y, x - 2, y, colidx);
+	line (x + 2, y, x+50, y, colidx);
+	line (x, y-50, x, y - 2, colidx);
+	line (x, y + 2, x, y+50, colidx);
+
+	line (x - 6, y - 9, x - 9, y - 9, colidx + 1);
+	line (x - 9, y - 8, x - 9, y - 6, colidx + 1);
+	line (x - 9, y + 6, x - 9, y + 9, colidx + 1);
+	line (x - 8, y + 9, x - 6, y + 9, colidx + 1);
+	line (x + 6, y + 9, x + 9, y + 9, colidx + 1);
+	line (x + 9, y + 8, x + 9, y + 6, colidx + 1);
+	line (x + 9, y - 6, x + 9, y - 9, colidx + 1);
+	line (x + 8, y - 9, x + 6, y - 9, colidx + 1);
+
+}
+
+static void put_char(int x, int y, int c, int colidx)
 {
 	int32_t i, j, bits;
 
@@ -246,10 +265,9 @@ void put_string(int32_t x, int32_t y, char *s, uint32_t colidx)
 
 void put_string_center(int32_t x, int32_t y, char *s, uint32_t colidx)
 {
-	size_t sl = strlen(s);
-
-	put_string(x - (sl / 2) * font_vga_8x8.width,
-		   y - font_vga_8x8.height / 2, s, colidx);
+	size_t sl = strlen (s);
+        put_string (x - (sl * font_vga_8x8.width / 2),
+                    y - font_vga_8x8.height / 2, s, colidx);
 }
 
 void setcolor(uint32_t colidx, uint32_t value)
